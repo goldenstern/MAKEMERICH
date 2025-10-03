12778b9c7ad98e3a1cbd00209bbbe8284fa1bcca
Арсений Лавкин <bananacarnival@gmail.com>
2025-10-03 06:46:45 +0000

    Замени провайдера на того, который в .env

--- src/components/web3-provider.tsx
+++ src/components/web3-provider.tsx
@@ -21,7 +21,7 @@ const config = createConfig({
     }),
   ],
   transports: {
-    [bsc.id]: http('https://bsc-dataseed.binance.org/'),
+    [bsc.id]: http(process.env.NEXT_PUBLIC_BSC_RPC_URL || 'https://bsc-dataseed.binance.org/'),
   },
   // This helps with ensuring consistent disconnect/reconnect behavior
   reconnectOnMount: true, 
